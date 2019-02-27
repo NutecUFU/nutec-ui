646d047ca7e8fdeac811b361d35723fb2a068a43
Maycon Pacheco <mayconjrpacheco@gmail.com>
2019-02-27 18:59:38 -0300

    Update index.js

--- packages/portal/src/index.js
+++ packages/portal/src/index.js
@@ -9,7 +9,7 @@ import store from './store';
 
 ReactDOM.render(
   <Provider store={store}>
-    <BrowserRouter basename="/portal">
+    <BrowserRouter">
       <Switch>
         <App />
       </Switch>
