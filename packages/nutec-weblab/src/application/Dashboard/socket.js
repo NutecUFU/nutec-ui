0e56cbd65e3f9e832c85a95cf1ed5cfb824d6f81
mayconjrpacheco <mayconjrpacheco@gmail.com>
2019-05-06 00:08:36 -0300

    Modify classroom socket types

--- packages/nutec-weblab/src/application/Dashboard/socket.js
+++ packages/nutec-weblab/src/application/Dashboard/socket.js
@@ -1,13 +1,13 @@
 import baseSocket from 'core/api/baseSocket'
 
-export const addUser = (name, control=false, isMaestro=false) => {
-  baseSocket.emit('user_join', { name, control, isMaestro })
+export const addUser = (name='', control=false, isMaestro=false, isSchedule=false) => {
+  baseSocket.emit('user.join', { name, control, isMaestro, isSchedule})
 }
 
 export const userJoined = (cb) => {
-  baseSocket.on('user_added', cb)
+  baseSocket.on('user.added', cb)
 } 
 
 export const getUsers = (cb) => {
-  baseSocket.on('user_list', cb)
+  baseSocket.on('user.list', cb)
 }
--- packages/nutec-weblab/src/experiments/LittleHouse/LivingRoom/styled.js
+++ packages/nutec-weblab/src/experiments/LittleHouse/LivingRoom/styled.js
@@ -2,6 +2,7 @@ import styled from 'styled-components'
 
 export const LivingRoomWrapper = styled.div`
   height: 100%;
+  margin-top: 60px;
 `
 
 export const FooterWrapper = styled.div`
